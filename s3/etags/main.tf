8ee7fda5a0f8f462398a6420716c7579beae3be8
bayasgalan-kh kh <bayasgalan.kh@gmail.com>
2025-02-21 01:09:04 +0000

    Etag examples

---
+++ s3/etags/main.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.30.0"
+        }
+    }
+}
+
+resource "aws_s3_bucket" "default" {}
+
+provider "aws" {
+
+}
+
+resource "aws_s3_object" "object" {
+    bucket = aws_s3_bucket.default.id
+    key = "myfile.txt"
+    source = "myfile.txt"
+    etag = filemd5("myfile.txt")
+}
